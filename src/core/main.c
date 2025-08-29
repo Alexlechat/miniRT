ab6971dde28a22e124ad2c091d37405a25706368
Anto <antoninpicard0@gmail.com>
2025-08-29 08:54:17 +0200

    fix: correct parse_file argument from argc to argv[1]

--- src/core/main.c
+++ src/core/main.c
@@ -6,7 +6,7 @@
 /*   By: anpicard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 14:26:00 by allefran          #+#    #+#             */
-/*   Updated: 2025/08/28 17:12:16 by anpicard         ###   ########.fr       */
+/*   Updated: 2025/08/29 08:53:37 by anpicard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 	display.width = 800;
 	if (argc != 2)
 		return (print_error("Usage : ./miniRT <scene.rt>\n", 2));
-	if (!parse_file(argc, argv[1]))
+	if (!parse_file(argv[1]))
 		return (print_error("Error\n", 2));
 	init_window(&display);
 	mlx_hook(display.window, EVENT_CLOSE, 0, close_window, &display);
